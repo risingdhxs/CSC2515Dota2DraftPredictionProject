9608e6ccb7d2741f78b06a995d39b7ba7e51195e
Xiaoshi (Gary) Huang <garyxshuang@gmail.com>
2018-12-06 01:30:18 -0500

    AutoEncoder_test
    testing environment; implemented sigmoid AE as well as weighted relu

---
+++ algorithms/AutoEncoder_test/TrainAE_2_test.py
@@ -0,0 +1,7 @@
+from trainautoencoder_2 import trainautoencoder_2
+from trainautoencoder_226 import trainautoencoder_226
+
+set = 'VH'
+# set = 'N_small'
+trainautoencoder_226([200, 160, 120, 100], set)
+# trainautoencoder_2([200, 180, 160, 140, 120, 100], set)
